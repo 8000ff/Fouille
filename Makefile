ade652636e5547054478e7c48ab64b4f97a56fe8
Clement <clementmallejac@gmail.com>
2020-09-20 12:32:27 +0200

    Add html and screenshot collector suing chromium

--- Makefile
+++ Makefile
@@ -1,3 +1,12 @@
 #TODO: Create database connection or instanciation rule
-test_rss: database sampleRSSFeeds.txt rssItemCollector.py
+
+BCC=browserContentCollector
+
+test_rss: sampleRSSFeeds.txt rssItemCollector.py
 	cat sampleRSSFeeds.txt | python3 rssItemCollector.py
+
+test_content: sampleURLs.txt $(BCC)/$(BCC).js
+	cat sampleURLs.txt | node $(BCC)/$(BCC).js
+
+clean:
+	rm -rf *.png
---
+++ browserContentCollector/browserContentCollector.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const readline = require('readline');
+const { stdin } = require('process');
+const devices = puppeteer.devices;
+
+task = async urls => {
+    const browser = await puppeteer.launch()
+    const pages = await Promise.all(
+        urls.map(async url => {
+            const page = await browser.newPage()
+            //TODO: Make device emulation configurable
+            await page.emulate(devices['iPhone X'])
+            await page.goto(url,{waitUntil:'load'}).catch(reason => console.log('page timed out :'+url))
+            console.log(url)
+            return page
+        })
+    )
+
+    const contents = Promise.all(
+        pages.map(async page => await page.content())
+    )
+
+    //TODO: make image screenshot configurable
+    const screenshots = Promise.all(
+        pages.map(async page => {
+            //TODO: refine image storage
+            const path = page.url().replace(/\.|\/|\:/g, '') + ".png"
+            console.log(path)
+            await page.screenshot({ path })
+        })
+    )
+    await screenshots
+    await contents
+    await browser.close()
+}
+
+urls = []
+stdinInterface = readline.createInterface({input:stdin})
+stdinInterface.on('line', line => urls.push(line))
+stdinInterface.on('close',() => task(urls))

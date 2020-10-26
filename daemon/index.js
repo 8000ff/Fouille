c9d38fe4ec417ad7012b477660c3d857323e42c6
Clement <clementmallejac@gmail.com>
2020-10-26 16:12:46 +0100

    Add job merging

--- daemon/index.js
+++ daemon/index.js
@@ -3,22 +3,21 @@ const { spawn } = require('child_process');
 
 const { MongoClient, ObjectID } = require("mongodb");
 
-function merge(a, b) {
-    m = newx M
-    const hist = (acc, x) => acc.set(x, acc.get(x) ? acc.get(x) + 1 : 1) /*needs a new Map([]) to work as intended*/
-
-
-}
 
 (async() => {
     const uri = env.MONGO_URI
     const dbName = "rss"
-
+    console.log(uri)
     const mongoClient = await new MongoClient(uri, { useUnifiedTopology: true })
     await mongoClient.connect()
     const config = await (await mongoClient.db(dbName).collection("config")).findOne({ name: "daemon" })
     jobs = []
     async function planJobs() {
+        function removeRedundancy(array) {
+            const joinByKey = (acc, x) => acc.set(x[0], [ ... (acc.get(x[0]) || []) , ... x[1] ])
+            return [...array.reduce(joinByKey,new Map())].map(t => [t[0],[...new Set(t[1])]]);
+        }
+
         const rss_task = await mongoClient.db(dbName).collection("rss_task")
         const tasks = await rss_task.find({ enable: true }).toArray()
 
@@ -26,28 +25,21 @@ function merge(a, b) {
             const collection = await mongoClient.db(dbName).collection(task['collection'])
             const filter = JSON.parse(task['filter'])
             const items = await collection.find(filter).toArray()
-            jobs.push([task.command, items.map(x => x['_id'])])
-            jobs = jobs.hist() // Merge same jobs
-
+            jobs.push([task.command, items.map(x => x['_id'].toString())])
+            jobs = removeRedundancy(jobs)
         })
     }
+    async function processJob(job){
+        
+    }
+    const queueJob = (opts) => {
+        if (t = jobs.pop()) {
+            return t(opts).pl.then(() => queueJob(opts))
+        }
+    }
 
-
-    //const rss_item = await mongoClient.db(dbName).collection("rss_item")
-
-    // console.log(tasks)
-
-    await Promise.all(await planJobs())
-    console.log(jobs)
-        // const simJob = 5
-        // const idleTime = 3000
-
-    // const queueJob = (job) => {
-    //     if (t = tasks.pop()) {
-    //         return t().then(queueJob)
-    //     } else {
-    //         return new Promise((resolve, reject) => setTimeout(resolve, idleTime)).then(queueJob)
-    //     }
-    // }
+    await planJobs()
+    await Promise.all(Array(batchSize).fill(queueJob).map((f, n) => f({ n })))
+        
     await mongoClient.close()
 })()
